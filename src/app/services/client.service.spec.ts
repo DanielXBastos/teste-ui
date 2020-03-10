f7e2ed1dcc4860ee0ddc790cd20cea430d96c61b
uzualdaniel <uzualdaniel@outlook.com>
2020-03-09 23:29:35 -0300

    inicianto teste em serviços

--- src/app/services/client.service.spec.ts
+++ src/app/services/client.service.spec.ts
@@ -1,16 +1,37 @@
+import { Client } from 'src/app/models/client';
 import { TestBed } from '@angular/core/testing';
-
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { ClientService } from './client.service';
 
 describe('ClientService', () => {
-  let service: ClientService;
+  let testService: ClientService;
+  let httpMock: HttpTestingController;
+  let testClient: Client;
 
   beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ClientService]
+    });
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ClientService);
+    testService = TestBed.inject(ClientService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('getData() should http GET clients', () => {
+
+    const names = [{ name: 'a' }, { name: 'a' }];
+
+    testService.getClients().subscribe((res) => {
+      console.log(res);
+      expect(res).toContain(testClient);
+    });
+
+    const req = httpMock.expectOne('/client');
+    // expect(req).toBeDefined();
+    // expect(req.request.method).toEqual('GET');
+    // req.flush(names);
+
+    // httpMock.verify();
   });
 });
